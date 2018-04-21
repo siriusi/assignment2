f24144b83c9272645264c8354143b2daaab2c745
siriusi <z.tomcato@gmail.com>
2018-04-21 13:10:38 +0800

    加上了自己的squeeze_net

--- squeeze_net_Old.py
+++ squeeze_net_Old.py
@@ -4,22 +4,13 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
 
-flags.DEFINE_integer('batch_size', 100, 'batch size')
-flags.DEFINE_integer('num_epochs', 35, 'number of epochs')
-flags.DEFINE_float('learning_rate', 0.04, 'init learning rate')
-flags.DEFINE_float('dropout', 0.5, 'define dropout keep probability')
-flags.DEFINE_float('max_grad_norm', 5.0, 'define maximum gradient normalize value')
-flags.DEFINE_float('normalize_decay', 5.0, 'batch normalize decay rate')
-flags.DEFINE_float('weight_decay', 0.0002, 'L2 regularizer weight decay rate')
-
-flags.DEFINE_integer('print_every', 5, 'how often to print training status')
-flags.DEFINE_string('name', None, 'name of result save dir')
 
 class SqueezeNet:
-    def __init__(self, img_shape, num_classes, normalize_decay = 0.999, weight_decay = 0.0002, clip_norm = 5.0):
+    def __init__(self, img_shape, num_classes):
+        FLAGS = tf.flags.FLAGS
         self.num_classes = num_classes
-        self.normalize_decay = normalize_decay
-        self.weight_decay = weight_decay
+        self.normalize_decay = FLAGS.normalize_decay
+        self.weight_decay = FLAGS.weight_decay
         self.learning_rate = tf.placeholder(tf.float32)
         self.dropout = tf.placeholder(tf.float32)
         # batch data & labels
@@ -36,20 +27,14 @@ class SqueezeNet:
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        tvars = tf.trainable_variables() #权重集，相当于W和b
+        tvars = tf.trainable_variables()
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
-        
-        #Gradient Clipping的引入是为了处理gradient explosion或者gradients vanishing的问题。
-        #Gradient Clipping的作用就是让权重（W或b）的更新限制在一个合适的范围。
-        #Gradient Clipping计算所有权重梯度的平方和sumsq_diff，当scale_factor = clip_norm  / sumsq_diff <1 时将所有权重乘以scale_factor
-        grads, global_norm = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), clip_norm)
-        
-        #将梯度应用在权重上
+        grads, global_norm = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), FLAGS.max_grad_norm)
         self.train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step)
-        
+
     def inference(self, scope='squeeze_net'):  # inference squeeze net
         with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
-            net = self.__conv2d(self.resized_data, 96, [3, 3], scope='conv_1') #[30, 30, 96]
+            net = self.__conv2d(self.resized_data, 96, [3, 3], scope='conv_1')
             net = layers.max_pool2d(net, [3, 3], scope='max_pool_1')
             net = self._fire_module(net, 16, 64, scope='fire_2')
             net = self._fire_module(net, 16, 64, scope='fire_3')
@@ -65,24 +50,25 @@ class SqueezeNet:
             net = self.__conv2d(net, self.num_classes, [1, 1], scope='conv_10')
             net = layers.avg_pool2d(net, [3, 3], stride=1, scope='avg_pool_1')
             return tf.squeeze(net, [2], name='logits')
-        
+
     def _fire_module(self, input_tensor, squeeze_depth, expand_depth, scope=None):
         with tf.variable_scope(scope):
             squeeze_tensor = self.__squeeze(input_tensor, squeeze_depth)
             expand_tensor = self.__expand(squeeze_tensor, expand_depth)
         return expand_tensor
+
     def __conv2d(self, input_tensor, num_outputs, kernel_size, stride=1, scope=None, is_training=True):
-        #decay是指在求滑动平均时的衰减，就是前面的数据影响会小一点
-        #fused是一个融合了几个操作的bn，比普通bn速度快
-        return layers.conv2d(input_tensor, num_outputs, kernel_size, stride=stride, scope=scope,data_format="NHWC",
-                      weights_regularizer=layers.l2_regularizer(self.weight_decay), normalizer_fn=layers.batch_norm,
-                      normalizer_params={"decay" = self.normalize_decay, "fused" = True, 'is_training': is_training}
-        
+        # convolution layer with default parameter
+        return layers.conv2d(input_tensor, num_outputs, kernel_size, stride=stride, scope=scope,
+                             data_format="NHWC",
+                             weights_regularizer=layers.l2_regularizer(self.weight_decay),
+                             normalizer_fn=layers.batch_norm,
+                             normalizer_params={'is_training': is_training, 'fused': True, 'decay': self.normalize_decay})
 
     def __squeeze(self, input_tensor, squeeze_depth):
-        return self.__conv2d(input_tensor, squeeze_depth, [1, 1], stride=1, scope=None, is_training=True)
-                               
+        return self.__conv2d(input_tensor, squeeze_depth, [1, 1], scope='squeeze')
+
     def __expand(self, input_tensor, expand_depth):
-        expand_1X1 = __conv2d(input_tensor, expand_depth, [1, 1], stride=1, scope='expand_1X1')
-        expand_3X3 = __conv2d(input_tensor, expand_depth, [3, 3], stride=1, scope='expand_3X3')    
-        return tf.concat(expand_1X1, expand_3X3)                
+        expand_1by1 = self.__conv2d(input_tensor, expand_depth, [1, 1], scope='expand_1by1')
+        expand_3by3 = self.__conv2d(input_tensor, expand_depth, [3, 3], scope='expand_3by3')
+        return tf.concat([expand_1by1, expand_3by3], 3)
